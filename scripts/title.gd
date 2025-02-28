43d628d6630de0544f3f3cb4e61f6e9e086dad38
Rashnain <51191602+Rashnain@users.noreply.github.com>
2025-02-28 13:29:36 +0100

    improve title screen

--- scripts/title.gd
+++ scripts/title.gd
@@ -4,6 +4,11 @@ extends Node2D
 @onready var player_chooser : HSlider = $PlayerChooser
 
 
-func _on_play_button_pressed() -> void:
+func _ready() -> void:
+	if Game.nr_players:
+		player_chooser.value = Game.nr_players
+
+
+func _on_start_button_pressed() -> void:
 	Game.nr_players = int(player_chooser.value)
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
