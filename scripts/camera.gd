8d6f62ba108b072c9684b00ae838ba04eec87453
Rashnain <51191602+Rashnain@users.noreply.github.com>
2025-03-01 23:45:55 +0100

    camera: change cursor when dragging
    also reduce lerp weight

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -7,7 +7,10 @@ var target_position: Vector2 = position
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			Input.set_default_cursor_shape(Input.CursorShape.CURSOR_MOVE)
 			target_position -= event.relative
+		else:
+			Input.set_default_cursor_shape()
 
 
 func _process(delta: float) -> void:
@@ -20,4 +23,4 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_down"):
 		target_position.y += 50 * delta * 7.5
 
-	position = lerp(position, target_position, 10 * delta)
+	position = lerp(position, target_position, 7.5 * delta)
