30f30bc78b5257313d41e5856656bab7dfd03349
Rashnain <51191602+Rashnain@users.noreply.github.com>
2025-03-02 17:47:39 +0100

    change history height

--- scripts/scroll_container.gd
+++ scripts/scroll_container.gd
@@ -7,14 +7,14 @@ var is_up_to_date := true
 
 
 func _input(event: InputEvent) -> void:
-	if instructions.size.y > 225:
+	if instructions.size.y > size.y:
 		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			is_up_to_date = false
 
 
 func _process(_delta: float) -> void:
-	if instructions.size.y > 225:
+	if instructions.size.y > size.y:
 		if is_up_to_date:
-			set_v_scroll(instructions.size.y - 225)
-		elif get_v_scroll() == instructions.size.y - 225:
+			set_v_scroll(int(instructions.size.y - size.y))
+		elif get_v_scroll() == instructions.size.y - size.y:
 			is_up_to_date = true
